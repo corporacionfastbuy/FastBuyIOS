39dbeec9e15ac1e36b33628f3a08adabc140d831
Luis Ysla Riojas <luisyslariojas@gmail.com>
2020-02-21 10:44:14 -0800

    Ysla

---
+++ FastBuyIOS/FastBuyIOS/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  FastBuyIOS
+//
+//  Created by Luis on 2/21/20.
+//  Copyright © 2020 CORPORACION FASTBUY S.A.C. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
